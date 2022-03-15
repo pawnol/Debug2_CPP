7565d1f54812a06a7ee551ff13c792b8651c13ac
Nolan Pawelski <32725534+pawnol@users.noreply.github.com>
2022-03-14 21:06:31 -0500

    Add files via upload

---
+++ Debug2.cpp
@@ -0,0 +1,21 @@
+/*
+Debug 2
+Pawelski
+3/14/2022
+Please follow the instructions on the activity sheet for what needs
+to be completed.
+*/
+
+#include <iostream>
+
+int main()
+{
+    int number1, number2, sum;
+    cout << "Enter a number >> ";
+    cin << number1;
+    cout << "Enter another number >> ";
+    cin << number2;
+    number1 * number2 = sum;
+    cout << sum;
+    return 0;
+}
